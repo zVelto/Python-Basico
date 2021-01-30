51824ce2b2058131a0485446a1a18a243d8059c1
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-30 16:37:31 -0300

    Comentários de código em Python

---
+++ aula1/aula1.py
@@ -0,0 +1,17 @@
+"""
+comentário com aspas duplas
+print('Linha 1')
+print('Linha 2')
+# Falando sobre do que se trata essa linha
+# Outra linha
+print('Linha 3')
+"""
+
+'''
+Comentário com aspas simples
+print('Linha 4') 
+print('Linha 5')
+'''
+print('Linha 6')  # Comentário de linha
+# print('Linha 7')
+print('Linha 8')
