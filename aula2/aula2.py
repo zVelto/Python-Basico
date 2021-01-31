0fd9f6eefecd83c7724a769af209593fbb45f64a
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-31 17:10:17 -0300

    O comando print

---
+++ aula2/aula2.py
@@ -0,0 +1,12 @@
+# print(123456)
+# print('Welton', 'Carvalho', 'Outra coisa')
+print('Welton', 'Carvalho', sep='-', end='####')
+print('João', 'e', 'Maria', sep='-', end='\n')
+
+print('824', end='.')
+print('176', end='.')
+print('078', end='-')
+print('18')
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
