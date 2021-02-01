c4ff49799946cd595f0011e21b6eef1c30bb8f82
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-31 23:24:44 -0300

    String (texto) e aspas em Python

---
+++ aula3/aula3.py
@@ -0,0 +1,9 @@
+"""
+str -string
+"""
+
+print('Essa é uma "string" (str).')
+print("Essa é uma 'string' (str).")
+print("Esse é meu \"texto\" (str).")  # Caractere de escape
+print('Esse é meu \'texto\' (str).')  # Caractere de escape
+print(r'Esse é meu \n (str).')  # Caractere de escape
