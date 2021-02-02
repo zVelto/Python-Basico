d65dfff12e823d38e4511d51bbb950ef903d4c0e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-02 15:58:50 -0300

    Operadores aritméticos

---
+++ aula5/aula5.py
@@ -0,0 +1,19 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+"""
+print('Multiplicação * ', 10 * 10)  # Multiplicação
+print('Adição + ', 10 + 10)  # Adição
+print('Subtração - ', 10 - 10)  # Subtração
+print('Divisão / ', 10 / 10)  # Divsão
+
+print('Adição +', '5' + "a")
+print('Multiplicação *', 5 * "Welton")
+
+print('Welton' + ' ' + 'Carvalho tem ' + str(25) + ' anos')
+"""
+print(325 ** 9)
+
+print(10 % 3)
+
+print((5 + 2) * 10)
