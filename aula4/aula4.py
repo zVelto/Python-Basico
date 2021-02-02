bc7acb245fc83d50b2e9651c436d83ad71f15e9c
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-01 21:29:13 -0300

    Tipos de dados 'primitivos'

---
+++ aula4/aula4.py
@@ -0,0 +1,30 @@
+"""
+Tipos de dados
+str - string - textos 'Assim "Assim
+int - inteiro - 123456 0 -10 -20 -50
+float - real/ponto flutuante - 10.50 1.5 -10.10 - tem casas decimais
+bool - booleano/lógico - true/false 10 == 10
+"""
+
+"""
+print('Welton', type('Welton'))
+print(10, type(10))
+print(25.23, type(25.23))
+print('l' == 'L', type('L' == 'L'))
+print(bool(''))
+print('10', type('10'), type(int('10')))
+"""
+
+# string: nome
+print('Welton Luis Carvalho', type('Welton Luis Carvalho'))
+
+# idade: int
+print(25, type(25))
+
+# altura: float
+print(1.73, type(1.73))
+
+# è maior de idade 10 > 20
+print(25 > 18, type(25 > 18))
+
+
