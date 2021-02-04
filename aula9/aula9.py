18ba7f09342f47140de9142311ec284278717ef5
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-03 23:01:02 -0300

    Input: Entrada de dados do usuário

---
+++ aula9/aula9.py
@@ -0,0 +1,16 @@
+"""
+Entrada de dados
+"""
+"""
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+
+ano_nascimento = 2020 - int(idade)
+print()
+print(f'{nome} nasceu em {ano_nascimento}')
+"""
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+print(numero_1 + numero_2)
