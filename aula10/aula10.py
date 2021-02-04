c06ac33d436cd45ff1e6ce1a3d2950b864532ae7
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-04 18:15:26 -0300

    IF, ELIF e ELSE + Booleans

---
+++ aula10/aula10.py
@@ -0,0 +1,18 @@
+"""
+Condições IF, ELIF e ELSE
+
+if True:
+    print("verdadeiro")
+
+    num_1 = 2
+    num_2 = 4
+    print(num_1 + num_2)
+
+"""
+
+if False:
+    print("verdadeiro")
+elif True:
+    print('Agora é verdadeiro')
+else:
+    print("Não é verdadeiro")
