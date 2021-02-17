75e998b796d6b3ebe668b1f57fbe9d4c7e5c0546
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-17 13:18:55 -0300

    Índices e fatiamento de strings em Python

---
+++ aula18/aula18.py
@@ -0,0 +1,23 @@
+
+texto = 'Python S2'
+
+print(texto[2])
+
+# url = 'www.google.com.br/'
+# print((url[:-1]))
+
+nova_string = texto[2:6]
+nova_string2 = texto[:6]
+nova_string3 = texto[7:]
+nova_string4 = texto[-1]
+nova_string5 = texto[-9:-3]
+nova_string6 = texto[0:5:2]
+nova_string7 = texto[0::2]
+
+print(nova_string)
+print(nova_string2)
+print(nova_string3)
+print(nova_string4)
+print(nova_string5)
+print(nova_string6)
+print(nova_string7)
